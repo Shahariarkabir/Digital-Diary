d8f8cb9796f6ffdd3f99bc91c06eca68565e8467
Shahariar Kabir <35670754+Shahariarkabir@users.noreply.github.com>
2020-12-17 00:26:59 +0600

    LoginForm  Design
    Created password Protected Login  Form

---
+++ Login.Designer.cs
@@ -0,0 +1,177 @@
+﻿
+namespace DIgital_Diary
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.Loginbtn = new Guna.UI2.WinForms.Guna2Button();
+            this.txtpassword = new Guna.UI2.WinForms.Guna2TextBox();
+            this.txtusername = new Guna.UI2.WinForms.Guna2TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
+            this.guna2DragControl1 = new Guna.UI2.WinForms.Guna2DragControl(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Loginbtn
+            // 
+            this.Loginbtn.BorderColor = System.Drawing.Color.White;
+            this.Loginbtn.BorderRadius = 10;
+            this.Loginbtn.BorderThickness = 2;
+            this.Loginbtn.CheckedState.Parent = this.Loginbtn;
+            this.Loginbtn.CustomImages.Parent = this.Loginbtn;
+            this.Loginbtn.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(77)))), ((int)(((byte)(164)))));
+            this.Loginbtn.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.Loginbtn.ForeColor = System.Drawing.Color.White;
+            this.Loginbtn.HoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(66)))), ((int)(((byte)(123)))));
+            this.Loginbtn.HoverState.Parent = this.Loginbtn;
+            this.Loginbtn.Location = new System.Drawing.Point(469, 296);
+            this.Loginbtn.Name = "Loginbtn";
+            this.Loginbtn.ShadowDecoration.Parent = this.Loginbtn;
+            this.Loginbtn.Size = new System.Drawing.Size(274, 49);
+            this.Loginbtn.TabIndex = 5;
+            this.Loginbtn.Text = "Login";
+            this.Loginbtn.Click += new System.EventHandler(this.Loginbtn_Click);
+            // 
+            // txtpassword
+            // 
+            this.txtpassword.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(77)))), ((int)(((byte)(164)))));
+            this.txtpassword.BorderRadius = 10;
+            this.txtpassword.BorderThickness = 2;
+            this.txtpassword.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtpassword.DefaultText = "";
+            this.txtpassword.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtpassword.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtpassword.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtpassword.DisabledState.Parent = this.txtpassword;
+            this.txtpassword.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtpassword.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtpassword.FocusedState.Parent = this.txtpassword;
+            this.txtpassword.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.txtpassword.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtpassword.HoverState.Parent = this.txtpassword;
+            this.txtpassword.Location = new System.Drawing.Point(469, 228);
+            this.txtpassword.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.PasswordChar = '*';
+            this.txtpassword.PlaceholderText = "Password";
+            this.txtpassword.SelectedText = "";
+            this.txtpassword.ShadowDecoration.Parent = this.txtpassword;
+            this.txtpassword.Size = new System.Drawing.Size(274, 49);
+            this.txtpassword.TabIndex = 4;
+            // 
+            // txtusername
+            // 
+            this.txtusername.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(77)))), ((int)(((byte)(164)))));
+            this.txtusername.BorderRadius = 10;
+            this.txtusername.BorderThickness = 2;
+            this.txtusername.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtusername.DefaultText = "";
+            this.txtusername.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtusername.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtusername.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtusername.DisabledState.Parent = this.txtusername;
+            this.txtusername.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtusername.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtusername.FocusedState.Parent = this.txtusername;
+            this.txtusername.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.txtusername.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtusername.HoverState.Parent = this.txtusername;
+            this.txtusername.Location = new System.Drawing.Point(469, 158);
+            this.txtusername.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtusername.Name = "txtusername";
+            this.txtusername.PasswordChar = '\0';
+            this.txtusername.PlaceholderText = "username";
+            this.txtusername.SelectedText = "";
+            this.txtusername.ShadowDecoration.Parent = this.txtusername;
+            this.txtusername.Size = new System.Drawing.Size(274, 49);
+            this.txtusername.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("JetBrains Mono Medium", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(66)))), ((int)(((byte)(123)))));
+            this.label1.Location = new System.Drawing.Point(464, 114);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 26);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Login";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(29, 86);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(410, 249);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // guna2Elipse1
+            // 
+            this.guna2Elipse1.TargetControl = this;
+            // 
+            // guna2DragControl1
+            // 
+            this.guna2DragControl1.TargetControl = this;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Loginbtn);
+            this.Controls.Add(this.txtpassword);
+            this.Controls.Add(this.txtusername);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Login";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Button Loginbtn;
+        private Guna.UI2.WinForms.Guna2TextBox txtpassword;
+        private Guna.UI2.WinForms.Guna2TextBox txtusername;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse1;
+        private Guna.UI2.WinForms.Guna2DragControl guna2DragControl1;
+    }
+}
+
---
+++ Login.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DIgital_Diary
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Loginbtn_Click(object sender, EventArgs e)
+        {
+
+            if (txtusername.Text == "kabir" && txtpassword.Text == "pass")
+            {
+                Dashboard ds = new Dashboard();
+                ds.Show();
+            }
+            else
+            {
+                txtpassword.Clear();
+
+            }
+        }
+    }
+}
