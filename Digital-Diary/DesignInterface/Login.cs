2671722f4a06ef0ce90ffd5bd86c5c3cf536b771
Shahariar Kabir <35670754+Shahariarkabir@users.noreply.github.com>
2020-12-19 22:58:25 +0600

    Login Page Connect  With  Database
    Database Create

---
+++ Digital-Diary/DesignInterface/Login.cs
@@ -0,0 +1,47 @@
+﻿using DIgital_Diary.BusinessLayer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DIgital_Diary
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Loginbtn_Click(object sender, EventArgs e)
+        {
+            if (UserNameTextBox.Text == "" || PasswordTextBox.Text == "")
+            {
+                MessageBox.Show("Username or password can not be empty");
+            }
+            else
+            {
+                UserService userService = new UserService();
+                bool result = userService.LoginValidation(UserNameTextBox.Text, PasswordTextBox.Text);
+                if (result)
+                {
+                    Dashboard dashBoard = new Dashboard();
+                    dashBoard.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Invalid Username or password");
+                }
+            }
+
+
+        }
+   
+    }
+}
